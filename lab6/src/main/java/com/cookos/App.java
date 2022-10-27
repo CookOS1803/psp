52c2a51bf62c7ca9f9c9bae838c49e4679d44cbf
CookOS1803 <usovich5321@gmail.com>
2022-10-28 00:12:33 +0300

    df

---
+++ lab6/src/main/java/com/cookos/App.java
@@ -0,0 +1,30 @@
+package com.cookos;
+
+import org.hibernate.*;
+import org.hibernate.boot.*;
+import org.hibernate.boot.registry.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+          
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+       
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+        Session session = factory.openSession();  
+        Transaction t = session.beginTransaction();   
+                 
+        Employee e1=new Employee();    
+        e1.setId(101);    
+        e1.setFirstName("Gaurav");    
+        e1.setLastName("Chawla");    
+            
+        session.save(e1);  
+        t.commit();  
+        System.out.println("successfully saved");    
+        factory.close();  
+        session.close();   
+    }
+}
