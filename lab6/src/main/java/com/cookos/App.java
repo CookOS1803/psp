57ccbea9bfdde05e7ebb66f86c2062dc8b915073
CookOS1803 <usovich5321@gmail.com>
2022-10-29 16:16:16 +0300

    ghv

--- lab6/src/main/java/com/cookos/App.java
+++ lab6/src/main/java/com/cookos/App.java
@@ -2,6 +2,7 @@ package com.cookos;
 
 import org.hibernate.boot.*;
 import org.hibernate.boot.registry.*;
+import org.hibernate.cfg.Configuration;
 
 public class App 
 {
@@ -11,13 +12,15 @@ public class App
         var meta = new MetadataSources(ssr).getMetadataBuilder().build();         
         var factory = meta.getSessionFactoryBuilder().build();  
         var session = factory.openSession();  
+        var configuration = new Configuration();
+        configuration.addAnnotatedClass(Employee.class);
         var transaction = session.beginTransaction();   
                  
         var e1 = new Employee();
         e1.setId(101);    
         e1.setFirstName("Gaurav");    
         e1.setLastName("Chawla");    
-            
+        session.delete(args);
         session.save(e1);  
         transaction.commit();
         factory.close();  
