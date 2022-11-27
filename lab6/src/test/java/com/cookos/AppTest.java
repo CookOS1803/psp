57c37346e7b9ca0457b4cabc81074301012b8608
CookOS1803 <usovich5321@gmail.com>
2022-11-27 18:39:41 +0300

    ghty

--- lab6/src/test/java/com/cookos/AppTest.java
+++ lab6/src/test/java/com/cookos/AppTest.java
@@ -1,20 +1,76 @@
 package com.cookos;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AppTest {
+    
+    private static final String FIRST_NAME = "Test";
+    private static final String LAST_NAME = "Employee";
+    private static final String POSITION = "TestPosition";
+    private static final int SALARY = 322;
+    private static final int EXPIRIENCE = 322;
+
+    private static final String NEW_POSITION = "NewTestPosition";
+
+    private EmployeeDao dao;
+    private Employee employee;
+
+    @BeforeAll
+    public void configure() {
+        dao = new EmployeeDao();
+        
+        employee = new Employee();
+        employee.setFirstName(FIRST_NAME);
+        employee.setLastName(LAST_NAME);
+        employee.setPosition(POSITION);
+        employee.setSalary(SALARY);
+        employee.setExperience(EXPIRIENCE);
     }
+
+    @Test
+    @Order(1)
+    public void testAdd() {
+        int id = dao.addEmployee(employee);
+
+        var persistedEmployee = dao.findById(id);
+
+        assertEquals(employee, persistedEmployee, "Failed to add new employee");
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdate() {
+        employee.setPosition(NEW_POSITION);
+
+        dao.updateEmployee(employee);
+
+        var persistedEmployee = dao.findById(employee.getId());
+
+        assertEquals(employee, persistedEmployee, "Failed to update employee");
+    }
+
+    @Test
+    @Order(3)
+    public void testRemove() {
+        try {
+            dao.removeEmployee(employee.getId());
+        } catch (Exception e) {
+            fail("Failded to remove employee");
+        }
+    }
+
+    @AfterAll
+    public void cleanup() {
+        try {
+            dao.close();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
 }
